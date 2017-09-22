bac9c78a89e34b9b14c62a4f04f207bd81fd5f28
RyanYip9 <ryanyip99@gmail.com>
2017-09-22 14:59:42 -0400

    HW2

---
+++ Unity/Interactive Environment HW/Assets/StoColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoColor : MonoBehaviour {
+
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("s")) {
+			rend.material.color = Color.blue;
+		}
+
+	}
+}
+
