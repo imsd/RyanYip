317e452096df4eb083cff94cdead12187e94ae43
RyanYip9 <student@ent-v314-016.local>
2017-09-26 15:45:51 -0400

    mouse exercise

---
+++ Unity/Week 5/Assets/Batter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Batter : MonoBehaviour {
+
+	public float rotatePower;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("Space")) {
+			GameObject baseball = Instantiate (goto, transform.ImagePosition, Quaternion.identify);
+			baseball.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 1200f);
+		}
+	}
+}
---
+++ Unity/Week 5/Assets/MouseStuff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseStuff : MonoBehaviour {
+
+	public float forcePower = 1000f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnMouseDown() {
+		Vector3 forceDirection = Vector3.forward * forcePower;
+		GetComponent<Rigidbody> ().AddForce (forceDirection);
+	}
+}
