72e22483dfba262d345a1d856dfdc223e3744b9b
RyanYip9 <student@ent-v314-016.local>
2017-10-05 15:55:36 -0400

    video experiment

---
+++ Unity/Week 6 - Video Experiment/Assets/rotate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+
+	public Vector3 myRotation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (myRotation * Time.deltaTime);
+		
+	}
+}
---
+++ Unity/Week 6 - Video Experiment/readme.md
@@ -0,0 +1,6 @@
+# Project1: Ryan Yip
+
+Description
+Instructions
+
+![The project in action](proj1.gif)
