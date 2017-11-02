e84db85ef01799c715532e9af426dc58aea11e1e
RyanYip9 <student@ent-v314-016.local>
2017-11-02 15:46:45 -0400

    Renaming and assignment work

---
+++ Unity/MadLibAssignment/Assets/Story.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Story : MonoBehaviour {
+
+	public string[] Hero;
+	public string[] Sidekick;
+	public string[] Villian;
+
+	// Use this for initialization
+	void Start () {
+		
+		string randHero = Random.Range (0, Hero.Length);
+
+		Debug.Log ("In a world filled with people who had the power to call themselves hero, " +
+		"one girl stood against the tide. " + Hero [randHero] + "had dreamed of becoming a hero. " + 
+		"But no one would recognize her because she was like 12, and a girl.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
