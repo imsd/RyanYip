75b94a5835dd51312646a4a44f744b01621552eb
RyanYip9 <student@ent-v314-016.local>
2017-11-02 15:32:22 -0400

    Week10

---
+++ Unity/Mad Libs/Assets/ArrayString.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayString : MonoBehaviour {
+
+	public string[] names;
+	public string[] places;
+
+	// Use this for initialization
+	void Start () {
+//		for (int c = 0; c < names.Length; c++) {
+//			Debug.Log (names [c]);
+//		}
+
+		int randNoun = Random.Range (0, names.Length);
+		int randPlace = Random.Range (0, places.Length);
+
+		string line1 = "One day, " + names [randNoun] + " went to " + places[randPlace] + ".";
+
+
+		Debug.Log (line1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		int rando = Random.Range (0, names.Length);
+//		Debug.Log (names [rando]);
+	}
+}
