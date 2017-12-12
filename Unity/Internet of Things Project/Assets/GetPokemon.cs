bb18708e742b6753de52b9ee5e124fb076bbafa9
RyanYip9 <student@ent-v314-016.local>
2017-12-12 15:45:05 -0500

    did stuff

--- Unity/Internet of Things Project/Assets/GetPokemon.cs
+++ Unity/Internet of Things Project/Assets/GetPokemon.cs
@@ -8,6 +8,17 @@ public class GetPokemon : MonoBehaviour {
 	public string pokemon = "bulbasaur";
 	public string imgUrl = "";
 
+
+	public int hp;
+	public int att;
+	public int def;
+	public int satt;
+	public int sdef;
+	public int spd;
+	public string tp;
+	public string tp1;
+
+
 	IEnumerator Start() {
 
 		// fetch the actual info, like you would from a browser
@@ -34,31 +45,73 @@ public class GetPokemon : MonoBehaviour {
 		// to get even more specific
 		JSONObject baseSpeed = speedDetails["base_stat"];
 		Debug.Log (baseSpeed);
+		spd = baseSpeed;
+
 
 		JSONObject specialdefenseDetails = statsDetails[1];
 		JSONObject baseSpecialDefense = specialdefenseDetails["base_stat"];
 		Debug.Log (baseSpecialDefense);
+		sdef = baseSpecialDefense;
 
 		JSONObject specialattackDetails = statsDetails[2];
 		JSONObject baseSpecialAttack = specialattackDetails["base_stat"];
 		Debug.Log (baseSpecialAttack);
+		satt = baseSpecialAttack;
 
 		JSONObject defenseDetails = statsDetails[3];
 		JSONObject baseDefense = defenseDetails["base_stat"];
 		Debug.Log (baseDefense);
+		def = baseDefense;
 
 		JSONObject attackDetails = statsDetails[4];
 		JSONObject baseAttack = attackDetails["base_stat"];
 		Debug.Log (baseAttack);
+		att = baseAttack;
 
 		JSONObject hpDetails = statsDetails[5];
 		JSONObject baseHp = hpDetails["base_stat"];
 		Debug.Log (baseHp);
+		hp = baseHp;
 
 		// get the sprite
 		JSONObject spriteDetails = tempData["sprites"];
 		JSONObject image = spriteDetails["front_default"];
 		Debug.Log (image);
-		imgUrl = image;
+		// for some reason, backslashes are getting introduced, probably in the string
+		// conversion, so we have to remove them here...
+		string imageURL = image.ToString ().Replace(@"\", "");
+
+		// AND its putting quotes in front and back, so remove those...
+
+		imageURL = imageURL.Replace ("\"", "");
+
+		// ... finally, time for the image! we have a clean URL
+			// Start a download of the given URL
+			WWW imageWWW = new WWW(imageURL);
+
+		// Wait for download to complete
+		yield return imageWWW;
+
+		// assign texture
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = imageWWW.texture;
+
+
+
+		JSONObject typeDetails = tempData["types"];
+		JSONObject type = typeDetails[0];
+		JSONObject baseType = type["type"];
+		JSONObject typeName = baseType["name"];
+		Debug.Log (typeName);
+		tp = typeName;
+
+		if (typeDetails [1] != null)
+		{
+			JSONObject atype = typeDetails [1];
+			JSONObject abaseType = atype ["type"];
+			JSONObject atypeName = abaseType ["name"];
+			Debug.Log (atypeName);
+			tp1 = atypeName;
+		}
 	}
 }
---
+++ Unity/Internet of Things Project/Assets/InfoDisplay.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoDisplay : MonoBehaviour {
+
+	public GameObject Here;
+	public string name;
+	public int hp;
+	public int att;
+	public int def;
+	public int satt;
+	public int sdef;
+	public int spd;
+	public string type;
+	public string type1;
+
+	public TextMesh derp;
+
+
+	// Use this for initialization
+	void Start () {
+		GameObject.GetComponent (GetPokemon);
+		this.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		name = GetPokemon.pokemon;
+		hp = GetPokemon.hp;
+		att = GetPokemon.att;
+		def = GetPokemon.def;
+		satt = GetPokemon.satt;
+		sdef = GetPokemon.sdef;
+		spd = GetPokemon.spd;
+		type = GetPokemon.tp;
+		if (GetPokemon.tp1 != null)
+		{
+			type1 = GetPokemon.tp1;
+		}
+
+		derp.text = name;
+	}
+}
